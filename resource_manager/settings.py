7a40d0cd51bda4e6fc712ff7eedb878f30916f23
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-01-31 18:55:28 -0300

    :wrench: chore: Rest Framework config

--- resource_manager/settings.py
+++ resource_manager/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'django_filters',
+    'rest_framework',
+
     "courses",
 ]
 
@@ -115,3 +118,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+
+    
+}
