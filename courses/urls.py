12a7896d21415edeaefada4c21234d6fe00079b5
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-02-01 02:03:57 -0300

    :sparkles: feat: overriding default methods "get_object" and "get_queryset"

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,10 @@ from .views import CourseAPIView, CoursesAPIView, ReviewAPIView, ReviewsAPIView
 
 urlpatterns = [
     path('cursos/', CoursesAPIView.as_view(), name='courses'),
-    path('avaliacoes/', ReviewsAPIView.as_view(), name='reviews'),
     path('cursos/<int:pk>/', CourseAPIView.as_view(), name='course'),
-    path('avaliacoes/<int:pk>/', ReviewAPIView.as_view(), name='review'),
+    path('cursos/<int:curso_pk>/avaliacoes/', ReviewsAPIView.as_view(), name='course_reviews'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', ReviewAPIView.as_view(), name='course_review'),
+
+    path('avaliacoes/', ReviewsAPIView.as_view(), name='reviews'),
+    path('avaliacoes/<int:avaliacao_pk>/', ReviewAPIView.as_view(), name='review'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework import generics
 
 from .models import Course, Review
@@ -16,6 +17,18 @@ class ReviewsAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(course_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 class ReviewAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                    course_id=self.kwargs.get('curso_pk'),
+                                    pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
